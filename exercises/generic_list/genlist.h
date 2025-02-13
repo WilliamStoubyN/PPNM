b33cc93e9770181d1295cc77ea79e3906f603520
William <202208144post.au.dk>
2025-02-13 12:51:28 +0100

    Added generic list exercise

---
+++ exercises/generic_list/genlist.h
@@ -0,0 +1,8 @@
+#ifndef HAVE_GENLIST_H
+#define HAVE_GENLIST_H
+
+template<class T>
+
+void add(T item);
+
+#endif
---
+++ exercises/generic_list/main.cc
@@ -0,0 +1,8 @@
+#include"genlist.h"
+#include<iostream>
+
+int main() {
+    //Task 1
+
+    return 0;
+}
